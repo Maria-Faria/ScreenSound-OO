e6c2dbc6504d23cd844d8a9ea355dec20d56640c
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-02-21 00:41:16 -0300

    criando a classe

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Song song1 = new Song();
+song1.name = "Love Hurts";
+song1.singer = "Nazareth";
+song1.duration = 273;
+song1.available = true;
+
+Song song2 = new Song();
+song2.name = "Cruel Summer";
+song2.singer = "Taylor Swift";
+song2.duration = 337;
+song2.available = false;
+
+song1.ExibirFichaTecnica();
+Console.WriteLine("----------------------");
+song2.ExibirFichaTecnica();
---
+++ Song.cs
@@ -0,0 +1,23 @@
+﻿class Song
+{
+    public string name;
+    public string singer;
+    public int duration;
+    public bool available;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {name}");
+        Console.WriteLine($"Artista: {singer}");
+        Console.WriteLine($"Duração: {duration}");
+    
+        if(available)
+        {
+            Console.WriteLine("Disponível no plano.");
+        
+        }else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
