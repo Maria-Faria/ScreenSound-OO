47186164fc00cd128f5378cd24ed8016dc8da011
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-04-16 23:55:34 -0300

    criando a lista de musicas

---
+++ Album.cs
@@ -0,0 +1,11 @@
+﻿class Album 
+{
+    private List<Song> songs = new List<Song>;
+    public string Name { get; set; }
+    public int Duration { get; set; }
+    
+    public void addSong(Song song)
+    {
+        songs.Add(song);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
-﻿Song song1 = new Song();
-song1.Name = "Love Hurts";
-song1.Singer = "Nazareth";
-song1.Duration = 273;
-song1.Available = true;
-Console.WriteLine(song1.Description);
+﻿Album imagineDragons = new Album();
+
+imagineDragons.Name = "Origins";
+
+Song song1 = new Song();
+song1.Name = "Natural";
 
 Song song2 = new Song();
-song2.Name = "Cruel Summer";
-song2.Singer = "Taylor Swift";
-song2.Duration = 337;
-song2.Available = false;
+song2.Name = "Machine";
 
-song1.ExibirFichaTecnica();
-Console.WriteLine("----------------------");
-song2.ExibirFichaTecnica();
+imagineDragons.addSong(song1);
+imagineDragons.addSong(song2);
